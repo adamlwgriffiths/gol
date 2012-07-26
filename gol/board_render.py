43e078a2e8c9cb7403df13add5403d6eafaa5981
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-07-27 00:51:12 +1000

    Initial commit.
    Numpy based Conway's Game of Life rules.
    Simple renderer with basic test harness.

---
+++ gol/board_render.py
@@ -0,0 +1,146 @@
+from pyglet.gl import *
+import numpy
+
+
+class BoardRenderer( object ):
+    
+    def __init__( self, board ):
+        super( BoardRenderer, self ).__init__()
+        
+        self.board = board
+        self.grid = self._create_grid( self.board )
+        self.cell = self._create_cell()
+
+    def render( self ):
+        # draw the grid
+        glLineWidth( 1.0 )
+        glColor3f( 1.0, 1.0, 1.0 )
+        glDisable( GL_LIGHTING )
+        
+        glCallList( self.grid )
+        
+        # draw any cells
+        glColor3f( 1.0, 0.0, 0.0 )
+        
+        indices = numpy.where( self.board == 1 )
+        indices = numpy.dstack( (indices[ 0 ], indices[ 1 ]) )[ 0 ]
+        
+        for cell in indices:
+            glLoadIdentity()
+            pos = numpy.array( [ float(cell[ 0 ]), float(cell[ 1 ]) ], dtype = numpy.float )
+
+            pos *= [ 2.0 / self.board.shape[ 0 ], 2.0 / self.board.shape[ 1 ] ]
+            pos += [-1.0,-1.0 ]
+            
+            glTranslatef( pos[ 0 ], pos[ 1 ], 0.0 )            
+
+            glCallList( self.cell )
+
+    def _create_cell( self ):
+        display_list = glGenLists( 1 )
+        glNewList( display_list, GL_COMPILE )
+        
+        self._draw_cell()
+        
+        glEndList()
+        
+        return display_list
+
+    def _draw_cell( self ):
+        glBegin( GL_QUADS )
+        
+        x_size, y_size = 2.0 / self.board.shape[ 0 ], 2.0 / self.board.shape[ 1 ]
+        
+        glVertex2f( 0.0, 0.0 )
+        glVertex2f( x_size, 0.0 )
+        glVertex2f( x_size, y_size )
+        glVertex2f( 0.0, y_size )
+        
+        glEnd()
+
+    def _create_grid( self, board ):
+        display_list = glGenLists( 1 )
+        glNewList( display_list, GL_COMPILE )
+        
+        self._draw_grid( board )
+        
+        glEndList()
+        
+        return display_list
+
+    def _draw_grid( self, board ):        
+        x_positions = numpy.array( [ x for x in range( board.shape[ 0 ] + 1 ) ], dtype = numpy.float )
+        y_positions = numpy.array( [ y for y in range( board.shape[ 1 ] + 1 ) ], dtype = numpy.float )
+        
+        min_x, max_x = float(x_positions.min()), float(x_positions.max())
+        min_y, max_y = float(y_positions.min()), float(y_positions.max())
+        
+        # we need to now splice these int x,y,z vertices
+        # we need 2 vertices per line
+        x_lines = numpy.dstack(
+            (
+                numpy.repeat( x_positions, 2 ),
+                [ min_y, max_y ] * len(x_positions),
+                [ 0.0, 0.0 ] * len(x_positions)
+                )
+            )
+        y_lines= numpy.dstack(
+            (
+                [ min_x, max_x ] * len(y_positions),
+                numpy.repeat( y_positions, 2 ),
+                [ 0.0, 0.0 ] * len(y_positions)
+                )
+            )
+        # join our two arrays together
+        vertices = numpy.concatenate( (x_lines, y_lines) )
+
+        # move the vertices so they are between -1.0 - 1.0
+        vertices *= [ 2.0 / board.shape[ 0 ], 2.0 / board.shape[ 1 ], 1.0 ]
+        vertices += [-1.0,-1.0,-10.0 ]
+        
+        vertices.shape = (-1,3)
+        
+        # render the vertices
+        glBegin( GL_LINES )
+        
+        for vertex in vertices:
+            #glVertex3f( vertex[ 0 ],vertex[ 1 ],vertex[ 2 ])
+            glVertex2f( vertex[ 0 ],vertex[ 1 ])
+        
+        glEnd()
+
+
+if __name__ == '__main__':
+    import rules_gol
+    
+    # Direct OpenGL commands to this window.
+    window = pyglet.window.Window()
+    
+    glEnableClientState(GL_VERTEX_ARRAY)
+    
+    board = numpy.random.randint( 2, size = (50,50) )
+    renderer = BoardRenderer( board )
+    
+    @window.event
+    def on_draw():
+        glClear(GL_COLOR_BUFFER_BIT)
+        glMatrixMode( GL_PROJECTION )
+        glLoadIdentity()
+        
+        glMatrixMode( GL_MODELVIEW )
+        glLoadIdentity()
+        global renderer
+        renderer.render()
+    
+    
+    def update( delta ):
+        global renderer
+        renderer.board = rules_gol.apply( renderer.board, wrap = (True, True) )
+    
+    # update our board at 1Hz
+    pyglet.clock.schedule_interval(
+        update,
+        1.0 / 30.0
+        )
+    
+    pyglet.app.run()
---
+++ gol/rules_gol.py
@@ -0,0 +1,95 @@
+import numpy
+
+def count_neighbours( cells, wrap ):
+    # we use "numpy.roll" to move the entire board at once
+    # ->
+    left = numpy.roll( cells, 1, axis = 0 )
+    # <-
+    right = numpy.roll( cells, -1, axis = 0 )
+    
+    # \/
+    up = numpy.roll( cells, 1, axis = 1 )
+    # ^
+    down = numpy.roll( cells, -1, axis = 1 )
+    
+    # up left
+    left_up = numpy.roll( left, 1, axis = 1 )
+    # up right
+    right_up = numpy.roll( right, 1, axis = 1 )
+    
+    # down left
+    left_down = numpy.roll( left, -1, axis = 1 )
+    # down right
+    right_down = numpy.roll( right, -1, axis = 1 )
+    
+    # check if wrapping is enabled
+    x_wrap = True if wrap != None and wrap[ 0 ] == True else False
+    y_wrap = True if wrap != None and wrap[ 1 ] == True else False
+ 
+    # if wrapping is not enabled
+    # we need to zero out the wrapped columns
+    if x_wrap == False:
+        # left
+        left[ 0, : ] = 0
+        left_up[ 0, : ] = 0
+        left_down[ 0, : ] = 0
+        
+        # right
+        right_up[-1, : ] = 0
+        right[-1, : ] = 0
+        right_down[-1, : ] = 0
+    if y_wrap == False:
+        # top
+        up[ :, 0 ] = 0
+        left_up[ :, 0 ] = 0
+        right_up[ :, 0 ] = 0
+        
+        # bottom
+        down[ :,-1 ] = 0
+        left_down[ :,-1 ] = 0
+        right_down[ :,-1 ] = 0
+    
+    counts = left + right + \
+        up + down + \
+        left_up + right_up + \
+        left_down + right_down
+    
+    return counts
+
+def apply( cells, wrap = None ):
+    assert cells.ndim == 2
+    
+    # we roll the cells around in 8 directions and add them up
+    # this lets us count ALL cells neighbours in one operation
+    counts = count_neighbours( cells, wrap )
+    
+    # apply conway's rules
+    # using numpy.where
+    
+    new_cells = numpy.array( cells )
+    
+    # rule 1: < 2 neighbour = dead
+    new_cells = numpy.where( counts < 2, [0], new_cells )
+    
+    # rule 2: 2 - 3 neighbours = alive
+    pass
+    
+    # rule 3: > 3 neighbours = dead
+    new_cells = numpy.where( counts > 3, [0], new_cells )
+    
+    # rule 4: 3 neighbours = alive
+    new_cells = numpy.where( counts == 3, [1], new_cells )
+    
+    return new_cells
+
+
+if __name__ == '__main__':
+    cells = numpy.random.randint( 2, size = (10,10) )
+    print "New cells"
+    print cells
+    
+    for i in range(5):
+        cells = apply( cells, wrap = (True, True) )
+        print "Generation ",i
+        print cells
+    pass
